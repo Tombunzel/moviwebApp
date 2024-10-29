398639ee88e7e241b7aa18f65ab79a87db0a8897
Tombunzel <bunzel.tom@gmail.com>
2024-10-29 12:38:17 +0100

    add docstrings and remove non-functional country flag functionality

--- datamanager/sqlite_data_manager.py
+++ datamanager/sqlite_data_manager.py
@@ -1,68 +1,46 @@
 from models import db, User, Movie
 from datamanager.data_manager_interface import DataManagerInterface
-from datamanager.api_fetcher import fetch_api_movie_dict, fetch_country_flag_url
+from datamanager.api_fetcher import fetch_api_movie_dict
 
 
 class SQLiteDataManager(DataManagerInterface):
+    """
+    SQL data manager class for all manipulations of the database.
+    """
     def __init__(self, app):
+        """Initialize db with the app
+        create tables if they don't exist"""
         self.db = db
-        # Initialize db with the app
         self.db.init_app(app)
         with app.app_context():
-            # Create tables if they don't exist
             self.db.create_all()
 
-    def add_favorite_movie(self, user_id, movie_id):
-        user = self.db.session.execute(
-            db.select(User)
-            .where(User.id == user_id)
-        ).scalars().one()
-        movie = self.db.session.execute(
-            db.select(Movie)
-            .where(Movie.id == movie_id)
-        ).scalars().one()
-
-        if not user or not movie:
-            raise ValueError("User or Movie not found")
-
-        # Add movie to user's favorites if it's not already in the list
-        if movie not in user.favorite_movies:
-            user.favorite_movies.append(movie)
-            self.db.session.commit()
-        else:
-            print("Movie already favorited by this user")
-
-    def get_user_favorites(self, user_id):
-        user = self.db.session.get(User, user_id)
-        if not user:
-            raise ValueError("User not found")
-
-        return user.favorite_movies  # Returns a list of movies favorited by the user
-
-    def get_movie_favorited_by(self, movie_id):
-        movie = self.db.session.get(Movie, movie_id)
-        if not movie:
-            raise ValueError("Movie not found")
-
-        return movie.favorited_by  # Returns a list of users who favorited this movie
-
     def get_all_users(self):
+        """returns all users in database"""
         return self.db.session.execute(self.db.select(User)).scalars().all()
 
     def get_all_movies(self):
+        """returns all movies in database"""
         return self.db.session.execute(self.db.select(Movie)).scalars().all()
 
     def get_user_movies(self, user_id):
+        """returns all movies of certain user"""
         return self.db.session.execute(
             self.db.select(Movie).where(Movie.user_favourite == user_id)
         ).scalars().all()
 
     def add_user(self, name):
+        """adds a user to the database"""
         new_user = User(name)
         self.db.session.add(new_user)
         self.db.session.commit()
 
     def add_movie(self, movie, user_id):
+        """
+        This function fetches information about a movie from its name,
+        then assigns that information to the movie object.
+        The movie is then added to the session and the addition commited.
+        """
         omdb_dict = fetch_api_movie_dict(movie.name)
         movie.rating = float(omdb_dict['Ratings'][0]['Value'].split('/')[0])
         movie.poster_url = omdb_dict['Poster']
@@ -76,6 +54,10 @@ class SQLiteDataManager(DataManagerInterface):
         self.db.session.commit()
 
     def update_movie(self, movie, info):
+        """
+        this function receives a movie (obj) and its info (dict)
+        finds the movie in the database and updates it with new info.
+        """
         update_fields = {k: v for k, v in info.items() if v is not None}
         if update_fields:
             self.db.session.execute(
@@ -84,9 +66,11 @@ class SQLiteDataManager(DataManagerInterface):
             self.db.session.commit()
 
     def delete_movie(self, movie_id):
+        """
+        this function deletes a movie from the database
+        according to movie_id
+        """
         self.db.session.execute(
             self.db.delete(Movie).where(Movie.id == movie_id)
         )
         self.db.session.commit()
-
-# print(fetch_api_movie_dict("The Green Mile"))
