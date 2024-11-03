18fe5c483d0bad7f987c31d5237d53fc94b4d5ff
Tombunzel <bunzel.tom@gmail.com>
2024-11-04 00:47:16 +0100

    add password, set and check methods

--- models.py
+++ models.py
@@ -1,20 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 # Initialize SQLAlchemy without attaching it to any app yet
 db = SQLAlchemy()
 
 
-class User(db.Model):
-    """
-    users table including id and name
-    and a one-to-many relationship with movies
-    """
-    __tablename__ = "users"
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String, nullable=False, unique=True)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     # Relationship to Movie
     movies = relationship("Movie", back_populates="user")
